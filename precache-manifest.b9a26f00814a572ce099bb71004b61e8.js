94ff00b9085ffef6ce485b03f6a5ab36c59543f6
Hala Alkhellow <halakhellow1996@gmail.com>
2020-09-01 22:29:27 +0300

    Updates

---
+++ precache-manifest.b9a26f00814a572ce099bb71004b61e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475bef9f0b801abb2468fe4b432349ef",
+    "url": "/color-boxes/index.html"
+  },
+  {
+    "revision": "f57c5340407eaaffd45e",
+    "url": "/color-boxes/static/css/main.620bfbb0.chunk.css"
+  },
+  {
+    "revision": "a4ff9fbbc95f60d731db",
+    "url": "/color-boxes/static/js/2.7503d2b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-boxes/static/js/2.7503d2b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f57c5340407eaaffd45e",
+    "url": "/color-boxes/static/js/main.e0c0df3d.chunk.js"
+  },
+  {
+    "revision": "485e4ed8fa72a7b3d07f",
+    "url": "/color-boxes/static/js/runtime-main.db813850.js"
+  }
+]);
